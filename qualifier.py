6920ef8649b7962ddbb66a4cf5498192ad1911f5
Laust <laustsf@gmail.com>
2020-07-31 23:42:56 +0200

    Finish Basic Requirement 1

--- qualifier.py
+++ qualifier.py
@@ -28,4 +28,16 @@ class Article:
     """The `Article` class you need to write for the qualifier."""
 
     def __init__(self, title: str, author: str, publication_date: datetime.datetime, content: str):
-        pass
+      self.title = title
+      self.author = author
+      self.publication_date = publication_date
+      self.content = content
+
+
+fairytale = Article(
+  title="The emperor's new clothes",
+  author="Hans Christian Andersen",
+  content="'But he has nothing at all on!' at last cried out all the people. The Emperor was vexed, for he knew that the people were right.",
+  publication_date=datetime.datetime(1837, 4, 7, 12, 15, 0),
+  )
+print(fairytale.title)
